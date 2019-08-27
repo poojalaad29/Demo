400050d2193a3b9b2c53dd4023ece3b9048f42bb
Pooja laad <Pooja laad@LAPTOP-ER3OQRDU>
2019-08-27 15:48:32 +0530

    First Commit

---
+++ src/Shape.java
@@ -0,0 +1,12 @@
+
+import com.Pooja.Shape.*;
+public class Shape {
+	public static void main(String [] ar) {
+	Circle obj=new Circle();
+  Cylinder obj1=new Cylinder();
+  double x=obj.Area(5);
+   double y=obj1.Area(5,7);
+   System.out.println(x);
+   System.out.println(y);
+	} 
+}
